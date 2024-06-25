7ec12475170a2731b31b3a2c97f7dada8d7e20b8
Duvindu Sahan <duvindusahan@gmail.com>
2024-06-25 20:43:42 +0530

    update home view

--- ExpenseTracker/home/views.py
+++ ExpenseTracker/home/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, logout
+from django.contrib.auth import login as dj_login
+from django.contrib.auth.models import User
+from .models import Addmoney_info, UserProfile
+from django.core.paginator import Paginator
+from django.db.models import Sum, Q
+import datetime
 
-# Create your views here.
+def home(request):
+    if request.session.has_key('is_logged'):
+        return redirect('/index')
+    return render(request, 'home/login.html')
