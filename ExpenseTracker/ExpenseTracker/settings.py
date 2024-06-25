3b4887e9a35fb6ed821329edcd69df7a57237f8c
Duvindu Sahan <duvindusahan@gmail.com>
2024-06-25 21:13:14 +0530

    final commit to settings.py

--- ExpenseTracker/ExpenseTracker/settings.py
+++ ExpenseTracker/ExpenseTracker/settings.py
@@ -128,3 +128,18 @@ STATICFILES_DIRS=[os.path.join(BASE_DIR,"static"),]
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MESSAGE_TAGS = {
+    messages.ERROR:"danger"
+}
+#SMTP Configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/img')
+MEDIA_URL = '/img/'
