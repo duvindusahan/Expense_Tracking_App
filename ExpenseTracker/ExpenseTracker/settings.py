714124c477f8a797356e3180369b18dc5f5b8b8a
Duvindu Sahan <duvindusahan@gmail.com>
2024-06-25 21:09:31 +0530

    adding home app to settings installed apps

--- ExpenseTracker/ExpenseTracker/settings.py
+++ ExpenseTracker/ExpenseTracker/settings.py
@@ -16,7 +16,7 @@ from django.contrib.messages import constants as messages
 from django.core import mail
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home.apps.HomeConfig',
 ]
 
 MIDDLEWARE = [
