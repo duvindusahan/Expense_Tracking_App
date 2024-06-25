7a643ef343c5af3dbfbfbf56f2053eb7c49187c0
Duvindu Sahan <duvindusahan@gmail.com>
2024-06-25 20:31:23 +0530

    Creating addmoney model

--- ExpenseTracker/home/models.py
+++ ExpenseTracker/home/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.utils.timezone import now
+from django.contrib.auth.models import User
 
-# Create your models here.
+SELECT_CATEGORY_CHOICES = [
+    ("Food", "Food"),
+    ("Travel", "Travel"),
+    ("Shopping", "Shopping"),
+    ("Necessities", "Necessities"),
+    ("Entertainment", "Entertainment"),
+    ("Other", "Other")
+]
+
+ADD_EXPENSE_CHOICES = [
+    ("Expense", "Expense"),
+    ("Income", "Income")
+]
+
+class Addmoney_info(models.Model):
+    user = models.ForeignKey(User, default=1, on_delete=models.CASCADE)
+    add_money = models.CharField(max_length=10, choices=ADD_EXPENSE_CHOICES)
+    quantity = models.BigIntegerField()
+    Date = models.DateField(default=now)
+    Category = models.CharField(max_length=20, choices=SELECT_CATEGORY_CHOICES, default='Food')
+    is_deleted = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = 'addmoney'
